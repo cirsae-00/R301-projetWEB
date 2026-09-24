123ab81f8c7ba921106ff5cf8a636f418975c626
l-L-E-N-A-l <lena.fougeron@etu.univ-amu.fr>
2026-09-24 15:51:53 +0200

    (fix) : fix du controller pour alwaysdata

--- _assets/includes/autoloader.php
+++ _assets/includes/autoloader.php
@@ -7,7 +7,7 @@ class Autoloader
     {
         spl_autoload_register(function ($class) {
 
-            $path = __DIR__ . '\\..\\..'.'\\' . 'modules\\' . lcfirst($class) . '.php';
+            $path = __DIR__ . DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'. DIRECTORY_SEPARATOR . 'modules'. DIRECTORY_SEPARATOR . lcfirst(str_replace('\\', DIRECTORY_SEPARATOR, $class)) . '.php';
             var_dump($path);
             if (file_exists($path)) {
                 require $path;
